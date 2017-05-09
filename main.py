88cf28994bc4a3d9f3986f902f211ee3928ff782
Michelle Li <michellecarolynclark@gmail.com>
2017-05-08 18:17:06 -0700

    separate train, test

--- main.py
+++ main.py
@@ -5,30 +5,29 @@ import pandas
 
 # Make a prediction with coefficients
 def predict(row, coefficients):
-	y_pred = coefficients[0]
+	u = coefficients[0] # bias (constant term)
 	for i in range(len(row)-1):
-		y_pred += coefficients[i + 1] * row[i]
-	return 1.0 / (1.0 + exp(-y_pred))
+		u += coefficients[i + 1] * row[i] # c_i * x_i
+	return 1.0 / (1.0 + exp(-u)) # sigmoid function
 
 # Estimate logistic regression coefficients using stochastic gradient descent
 def gradient_descent(X_train, Y_train, l_rate, n_epoch):
-	coef = [0.0 for i in range(len(X_train[0])+1)]
+	coefficients = [0.0 for i in range(len(X_train[0])+1)]
 	for epoch in range(n_epoch):
 		for row_ind in range(len(X_train)):
-			y_pred = predict(X_train[row_ind], coef)
+			y_pred = predict(X_train[row_ind], coefficients)
 			error = Y_train[row_ind] - y_pred
-			coef[0] = coef[0] + l_rate * error * y_pred * (1.0 - y_pred)
+			coefficients[0] = coefficients[0] + l_rate * error * y_pred * (1.0 - y_pred)
 			for i in range(len(X_train[row_ind])):
-				coef[i + 1] = coef[i + 1] + l_rate * error * y_pred * (1.0 - y_pred) * X_train[row_ind][i]
-	return coef
+				coefficients[i + 1] = coefficients[i + 1] + l_rate * error * y_pred * (1.0 - y_pred) * X_train[row_ind][i]
+	return coefficients
 
-# Linear Regression Algorithm With Stochastic Gradient Descent
-def logistic_regression(X_train, X_validation, Y_train, Y_validation, l_rate, n_epoch):
+# Linear Regression Algorithm
+def logistic_regression(X_validation, l_rate, n_epoch, coefficients):
 	predictions = list()
-	coef = gradient_descent(X_train, Y_train, l_rate, n_epoch)
+    # make predictions (validate)
 	for row in X_validation:
-		y_pred = predict(row, coef)
-		y_pred = round(y_pred)
+		y_pred = round(predict(row, coefficients))
 		predictions.append(y_pred)
 	return(predictions)
 
@@ -36,7 +35,7 @@ def logistic_regression(X_train, X_validation, Y_train, Y_validation, l_rate, n_
 filename = "data.csv"
 dataset = pandas.read_csv(filename).values
 
-# normalize
+# find max and min to normalize
 minmax = list()
 for i in range(len(dataset[0])):
     col_values = [row[i] for row in dataset]
@@ -44,6 +43,7 @@ for i in range(len(dataset[0])):
     value_max = max(col_values)
     minmax.append([value_min, value_max])
 
+# normalize values
 for row in dataset:
     for i in range(len(row)):
         row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
@@ -59,9 +59,14 @@ X_train, X_validation, Y_train, Y_validation = cross_validation.train_test_split
 # evaluate algorithm
 l_rate = 0.1
 n_epoch = 100
-predictions = logistic_regression(X_train, X_validation, Y_train, Y_validation, l_rate, n_epoch)
+# train the model
+coefficients = gradient_descent(X_train, Y_train, l_rate, n_epoch)
+# make predictions
+predictions = logistic_regression(X_validation, l_rate, n_epoch, coefficients)
+# determine accuracy
 correct = 0
 for i in range(len(Y_validation)):
     if Y_validation[i] == predictions[i]:
         correct += 1
-print(correct / float(len(Y_validation)) * 100.0)
+
+print('Accuracy:', correct / float(len(Y_validation)) * 100.0)
